d61a746d5120e5ee78268f448e4c1fc46e9e592d
ChristianT321 <thodeawesome@gmail.com>
2025-11-21 18:24:39 -0700

    Updated nav

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
+
 import ProductList from './components/ProductList';
+import ProductForm from './components/ProductForm';
+
 import WarehouseList from './components/WarehouseList';
+import WarehouseForm from './components/WarehouseForm';
+
 import TransactionList from './components/TransactionList';
+
 import Report from './components/Report';
+
 import { AuthProvider } from './contexts/AuthContext';
 import ProtectedRoute from './components/ProtectedRoute';
+
 import './styles.css';
 
 function App() {
@@ -15,12 +24,44 @@ function App() {
     <Router>
       <AuthProvider>
         <Routes>
+
           <Route path="/login" element={<Login />} />
-          <Route path="/" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
-          <Route path="/products" element={<ProtectedRoute><ProductList /></ProtectedRoute>} />
-          <Route path="/warehouses" element={<ProtectedRoute><WarehouseList /></ProtectedRoute>} />
-          <Route path="/transactions" element={<ProtectedRoute><TransactionList /></ProtectedRoute>} />
-          <Route path="/reports" element={<ProtectedRoute><Report /></ProtectedRoute>} />
+
+          <Route 
+            path="/" 
+            element={<ProtectedRoute><Dashboard /></ProtectedRoute>} 
+          />
+
+          <Route 
+            path="/products" 
+            element={<ProtectedRoute><ProductList /></ProtectedRoute>} 
+          />
+
+          <Route 
+            path="/products/new" 
+            element={<ProtectedRoute><ProductForm /></ProtectedRoute>} 
+          />
+
+          <Route 
+            path="/warehouses" 
+            element={<ProtectedRoute><WarehouseList /></ProtectedRoute>} 
+          />
+
+          <Route 
+            path="/warehouses/new" 
+            element={<ProtectedRoute><WarehouseForm /></ProtectedRoute>} 
+          />
+
+          <Route 
+            path="/transactions" 
+            element={<ProtectedRoute><TransactionList /></ProtectedRoute>} 
+          />
+
+          <Route 
+            path="/reports" 
+            element={<ProtectedRoute><Report /></ProtectedRoute>} 
+          />
+
         </Routes>
       </AuthProvider>
     </Router>
